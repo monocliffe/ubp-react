8dcb1579db49bf4ec37825085b185041bef070bc
Stephen <stephen.cliffe@wpengine.com>
2020-04-20 15:47:59 +0100

    Fixed issues with requests & added async request. Update ComponentDidMount to useEffect.

--- src/Posts.js
+++ src/Posts.js
@@ -1,31 +1,33 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
 
-export default class Posts extends React.Component {
-    state = {
-        posts: []
+export default function Posts(props) {
+    const [posts, setPosts] = useState([
+        {
+            posts: []
+        }
+    ]);
+
+    async function fetchPostsData() {
+        const response = await fetch(
+            `http://0.0.0.0:3000/posts.json`
+        );
+        setPosts(await response.json());
     }
 
-    componentDidMount() {
-        axios.get('http://0.0.0.0:3000/posts.json')
-            .then(res => {
-                const posts = res.data;
-                this.setState({ posts });
-            })
-    }
+    useEffect(() => {
+        fetchPostsData(props.id);
+    }, [props.id]);
 
-    render() {
-        return (
-            <ul>
-                { this.state.posts.map(post =>
-                    <div key={post.id}>
-                        <div>
-                            <h3>{post.title}</h3>
-                            <div dangerouslySetInnerHTML={{ __html: post.rich_body }}></div>
-                        </div>
+    return (
+        <ul>
+            { posts.map(post =>
+                <div key={post.id}>
+                    <div>
+                        <h3>{post.title}</h3>
+                        <div dangerouslySetInnerHTML={{__html: post.rich_body}}/>
                     </div>
-                )}
-            </ul>
-        )
-    }
+                </div>
+            )}
+        </ul>
+    );
 }
